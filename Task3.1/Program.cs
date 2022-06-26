b56c86197b927948674378f272d1f7097d79e09d
Toknnick <106018075+Toknnick@users.noreply.github.com>
2022-06-26 12:40:19 +0300

    Update Program.cs

--- Task3.1/Program.cs
+++ Task3.1/Program.cs
@@ -8,23 +8,18 @@ namespace Task3._1
         {
             int productOfFirstColumn = 1;
             int sumOfSecondLine = 0;
+            int numberOfLine = 1;
+            int numberOfColumn = 0;
             int[,] arrayOfNumbers = { { 9, 2, 3, 4, 5, 6, }, { 5, 3, 4, 5, 6, 7 } };
 
             for (int j = 0; j < arrayOfNumbers.GetLength(1); j++)
             {
-                int i = 1;
-                sumOfSecondLine += arrayOfNumbers[i, j];
+                sumOfSecondLine += arrayOfNumbers[numberOfLine, j];
             }
 
             for (int i = 0; i < arrayOfNumbers.GetLength(0); i++)
             {
-                for (int j = 0; j < arrayOfNumbers.GetLength(1); j++)
-                {
-                    if (j == 0)
-                    {
-                        productOfFirstColumn *= arrayOfNumbers[i, j];
-                    }
-                }
+                productOfFirstColumn *= arrayOfNumbers[i, numberOfColumn];
             }
 
             for (int i = 0; i < arrayOfNumbers.GetLength(0); i++)
@@ -33,10 +28,10 @@ namespace Task3._1
                 {
                     Console.Write(arrayOfNumbers[i, j] + " ");
                 }
-                
+
                 Console.WriteLine();
             }
-            
+
             Console.WriteLine($"\nСумма второй строки: {sumOfSecondLine}, Произведение первого столбика: {productOfFirstColumn}\n");
         }
     }
