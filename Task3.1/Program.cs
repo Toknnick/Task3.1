49a7caf88d6dda207f3118af1df444b99b57a251
Toknnick <106018075+Toknnick@users.noreply.github.com>
2022-06-26 11:35:07 +0300

    Update Program.cs

--- Task3.1/Program.cs
+++ Task3.1/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Task3._1
 {
@@ -10,36 +6,31 @@ namespace Task3._1
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int maxValue = 9;
-            int minValue = 1;
-            int numberOfLines = random.Next(minValue, maxValue); ;
-            int numberOfColumns = random.Next(minValue, maxValue);
-            int productOfFirstColumn = 1; 
+            int productOfFirstColumn = 1;
             int sumOfSecondLine = 0;
-            int[,] arrayOfNumbers = new int[numberOfLines, numberOfColumns];
-            Console.WriteLine($"Строк: {numberOfLines} столбов: {numberOfColumns}");
-            int numberMinArray = 10;
-            int numberMaxArray = 99;
-            Console.WriteLine("\nМассив:");
+            int[,] arrayOfNumbers = { { 1, 2, 3, 4, 5, 6, }, { 5, 3, 4, 5, 6, 7 } };
+
+            for (int j = 0; j < arrayOfNumbers.GetLength(1); j++)
+            {
+                int i = 1;
+
+                if (i == 1)
+                {
+                    sumOfSecondLine += arrayOfNumbers[i, j];
+                }
+
+                if (j == 0)
+                {
+                    productOfFirstColumn *= arrayOfNumbers[i, j];
+                }
+            }
 
             for (int i = 0; i < arrayOfNumbers.GetLength(0); i++)
             {
 
                 for (int j = 0; j < arrayOfNumbers.GetLength(1); j++)
                 {
-                    arrayOfNumbers[i, j] = random.Next(numberMinArray, numberMaxArray);
                     Console.Write(arrayOfNumbers[i, j] + " ");
-
-                    if (i == 1)
-                    {
-                        sumOfSecondLine += arrayOfNumbers[i, j];
-                    }
-
-                    if (j == 0)
-                    {
-                        productOfFirstColumn *= arrayOfNumbers[i, j];
-                    }
                 }
                 Console.WriteLine();
             }
